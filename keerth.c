bd450320c194b5323e15af2aad6ee577d6bc86da
keerthanaranganathan <keerthanagandhi5@gmail.com>
2016-09-02 11:09:23 +0530

    Create keerth.c

---
+++ keerth.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the no");
+scanf("%d",&n);
+if(n>0)
+{
+printf("positive number");
+}
+else if(n<0)
+{
+printf("negative number");
+}
+else
+{
+printf("the no is zero");
+}
+}
+
